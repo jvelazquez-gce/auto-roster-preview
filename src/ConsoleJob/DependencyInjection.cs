fbb6f17444a49a25e67953b4dbe9d33f78e52b89
jvelazquez-gce <161651444+jvelazquez-gce@users.noreply.github.com>
2025-06-04 12:21:38 -0700

    Add files via upload

---
+++ src/ConsoleJob/DependencyInjection.cs
@@ -0,0 +1,127 @@
+﻿using Domain.Configuration;
+using Domain.Interfaces.Infrastructure.Database.Commands;
+using Domain.Interfaces.Infrastructure.Database.Commands.LastClassGroups;
+using Domain.Interfaces.Infrastructure.Database.Queries;
+using Domain.Interfaces.Infrastructure.Database.Queries.LastClassGroupSection;
+using Domain.Interfaces.Infrastructure.Database.Queries.OneStudentPerSection;
+using Domain.Interfaces.Infrastructure.Database.Queries.RulesEngine;
+using Domain.Interfaces.Infrastructure.Database.Repositories;
+using Domain.Interfaces.Infrastructure.Utilities;
+using Infrastructure.Database.Commands;
+using Infrastructure.Database.Commands.LastClassGroups;
+using Infrastructure.Database.Context;
+using Infrastructure.Database.Queries;
+using Infrastructure.Database.Queries.LastClassGroupSection;
+using Infrastructure.Database.Queries.OneStudentPerSection;
+using Infrastructure.Database.Queries.RulesEngine;
+using Infrastructure.Database.Repositories;
+using Infrastructure.Utilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Calculators;
+using Services.CVue;
+using Services.Jobs;
+using Services.LastClassTogether;
+using Services.OneStudentPerSection;
+using Services.Utilities;
+
+namespace ConsoleJob
+{
+    public class DependencyInjection
+    {
+        static IConfigurationRoot Configuration { get; set; }
+        public AppSettings AppSettings { get; set; } = new AppSettings();
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            // Step 3: Register services with configuration (if needed)
+            var appSettings = new AppSettings();
+            Configuration.GetSection("AppSettings").Bind(appSettings);
+            services.AddSingleton<AppSettings>(appSettings);
+            AppSettings = appSettings;
+
+            services.AddApplicationInsightsTelemetryWorkerService(Configuration);
+
+            services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+            services.AddSingleton<IDbContextFactory>(sp =>
+            {
+                var connectionString = AppSettings.DbConnectionString;
+                return new DbContextFactory(connectionString);
+            });
+            services.AddTransient<ILogCommand, LogCommand>();
+            services.AddTransient<IJobRepository, JobRepository>();
+            services.AddSingleton<IConfigurationRepository, ConfigurationRepository>();
+            services.AddSingleton<IFeatureToggle, FeatureToggle>();
+
+            services.AddSingleton<IConfigInstance, ConfigInstance>();
+
+            services.AddTransient<IAddLastClassGroupStudentSectionsCommand, AddLastClassGroupStudentSectionsCommand>();
+            services.AddTransient<IDeleteAllLastClassGroupMissingFromPreLoadCommand, DeleteAllLastClassGroupMissingFromPreLoadCommand>();
+            services.AddTransient<IDisableLastClassGroupEmptySectionsCommand, DisableLastClassGroupEmptySectionsCommand>();
+            services.AddTransient<IRemoveLastClassGroupStudentSectionCommand, RemoveLastClassGroupStudentSectionCommand>();
+
+            services.AddTransient<IAddCourseSectionsCommand, AddCourseSectionsCommand>();
+            services.AddTransient<IAddCourseSectionsTxCommand, AddCourseSectionsTxCommand>();
+            services.AddTransient<IAddLiveModeStudentAndCourseSectionsCommand, AddLiveModeStudentAndCourseSectionsCommand>();
+            services.AddTransient<IAddNewSectionAndStudentRecordsForOneStudentPerSectionCommand, AddNewSectionAndStudentRecordsForOneStudentPerSectionCommand>();
+            services.AddTransient<IAddPreviewStudentAndUpdateCourseSectionsCommand, AddPreviewStudentAndUpdateCourseSectionsCommand>();
+            services.AddTransient<IAddPreviewStudentAndUpdateCourseSectionsCommand, AddPreviewStudentAndUpdateCourseSectionsCommand>();
+            services.AddTransient<IAddStudentSectionErrorCommand, AddStudentSectionErrorCommand>();
+            services.AddTransient<IAddStudentSectionErrorsCommand, AddStudentSectionErrorsCommand>();
+            services.AddTransient<IDeleteOrInactivateCVueSectionsTxCommand, DeleteOrInactivateCVueSectionsTxCommand>();
+            services.AddTransient<IPreRunDBSetUpCommand, PreRunDBSetUpCommand>();
+            services.AddTransient<IRemoveCourseSectionsWithoutSavingThemCommand, RemoveCourseSectionsWithoutSavingThemCommand>();
+            services.AddTransient<ISaveChangesCommand, SaveChangesCommand>();
+            services.AddTransient<IUpdateCourseSectionCommand, UpdateCourseSectionCommand>();
+            services.AddTransient<IUpdateCourseSectionWithoutSavingCommand, UpdateCourseSectionWithoutSavingCommand>();
+            services.AddTransient<IUpdateOneStudentPerSectionRecordsFromLiveRecordsNoSaveCommand, UpdateOneStudentPerSectionRecordsFromLiveRecordsNoSaveCommand>();
+            services.AddTransient<IUpdateOneStudentPerSectionRecordsToInactivateCommand, UpdateOneStudentPerSectionRecordsToInactivateCommand>();
+
+            services.AddTransient<IGetActiveLastClassGroupCourseSectionsToCreateQuery, GetActiveLastClassGroupCourseSectionsToCreateQuery>();
+            services.AddTransient<IGetClassesTogetherToGroupRulesQuery, GetClassesTogetherToGroupRulesQuery>();
+            services.AddTransient<IGetLastClassGroupStudentsToTransferQuery, GetLastClassGroupStudentsToTransferQuery>();
+
+            services.AddTransient<IGetActiveCVueOneStudentPerSectionCourseSectionsToCancelQuery, GetActiveCVueOneStudentPerSectionCourseSectionsToCancelQuery>();
+            services.AddTransient<IGetActiveOneStudentPerSectionCourseSectionsToCreateQuery, GetActiveOneStudentPerSectionCourseSectionsToCreateQuery>();
+            services.AddTransient<IGetOneStudentPerSectionCourseSectionsReadyToCreateOrCreatedQuery, GetOneStudentPerSectionCourseSectionsReadyToCreateOrCreatedQuery>();
+            services.AddTransient<IGetPreviewOneStudentPerSectionStudentsToTransferQuery, GetPreviewOneStudentPerSectionStudentsToTransferQuery>();
+
+            services.AddTransient<IGetActiveClassGroupCourseSectionsToCreateQuery, GetActiveClassGroupCourseSectionsToCreateQuery>();
+            services.AddTransient<IGetActiveRulesByCourseIdQuery, GetActiveRulesByCourseIdQuery>();
+            services.AddTransient<IGetClassGroupStudentsToTransferQuery, GetClassGroupStudentsToTransferQuery>();
+
+            services.AddTransient<IGetCourseSectionsThatCanBeReUsedByCourseIdAndStartDateQuery, GetCourseSectionsThatCanBeReUsedByCourseIdAndStartDateQuery>();
+            services.AddTransient<IGetPreLoadDataForPreviewQuery, GetPreLoadDataForPreviewQuery>();
+            services.AddTransient<IGetPreLoadDataForUpdateQuery, GetPreLoadDataForUpdateQuery>();
+
+            services.AddTransient<IGetPreLoadDataForUpdateQuery, GetPreLoadDataForUpdateQuery>();
+
+            services.AddTransient<IConfigurationRepository, ConfigurationRepository>();
+            services.AddTransient<IJobRepository, JobRepository>();
+            services.AddTransient<ISectionErrorRepository, SectionErrorRepository>();
+
+            services.AddTransient<IOneStudentPerSectionCourseSectionHandler, OneStudentPerSectionCourseSectionHandler>();
+            services.AddTransient<IOneStudentPerSectionCVueSectionsHandler, OneStudentPerSectionCVueSectionsHandler>();
+            services.AddTransient<IOneStudentPerSectionErrorChecking, OneStudentPerSectionErrorChecking>();
+            services.AddTransient<ICampusVueSetUpHandler, CampusVueSetUpHandler>();
+            services.AddTransient<ILastClassTogetherHandler, LastClassTogetherHandler>();
+            services.AddTransient<IOneStudentPerSectionHandler, OneStudentPerSectionHandler>();
+            services.AddTransient<INoGroupCalculator, NoGroupCalculator>();
+            services.AddTransient<IGroupCalculator, GroupCalculator>();
+            services.AddTransient<IGeneralLastClassTogetherCalculator, GeneralLastClassTogetherCalculator>();
+            services.AddTransient<ICVueSectionsCalculator, CVueSectionsCalculator>();
+            services.AddTransient<ICourseSectionCalculator, CourseSectionCalculator>();
+            services.AddTransient<IErrorChecking, ErrorChecking>();
+            services.AddTransient<ICalculator, Calculator>();
+            services.AddTransient<IBalancer, Balancer>();
+            services.AddTransient<ILoaderValidator, LoaderValidator>();
+            services.AddTransient<IJobService, JobService>();
+        }
+    }
+}
---
+++ src/ConsoleJob/Program.cs
@@ -0,0 +1,112 @@
+﻿using Domain.Configuration;
+using Domain.Constants;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces.Infrastructure.Database.Commands;
+using Domain.Interfaces.Infrastructure.Database.Repositories;
+using Infrastructure.Utilities;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Jobs;
+using System;
+using System.Linq;
+
+namespace ConsoleJob
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var appSettings = new AppSettings();
+            ILogCommand _logCommand = null;
+            TelemetryClient _telemetryClient;
+            IJobService _jobService = null;
+            IJobRepository _jobRepository = null;
+            try
+            {
+                var dependencyInjection = new DependencyInjection();
+                IServiceCollection services = new ServiceCollection();
+                dependencyInjection.ConfigureServices(services);
+                IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+                appSettings = dependencyInjection.AppSettings;
+
+                _logCommand = serviceProvider.GetService<ILogCommand>();
+                _telemetryClient = serviceProvider.GetService<TelemetryClient>();
+                _jobService = serviceProvider.GetService<IJobService>();
+                _jobRepository = serviceProvider.GetService<IJobRepository>();
+
+                using (var operation = _telemetryClient.StartOperation<RequestTelemetry>("Running ARB Preview."))
+                {
+                    try
+                    {
+                        var msg = $"Starting StartProcessing ";
+                        var log = MyLogger.GetDefaultLog(Guid.Empty, msg, -1, LogLevel.Info);
+                        _logCommand.Execute(log);
+
+                        _jobService.StartProcessing(appSettings);
+
+                        msg = $"Completed StartProcessing ";
+                        log = MyLogger.GetDefaultLog(Guid.Empty, msg, -1, LogLevel.Info);
+                        _logCommand.Execute(log);
+                    }
+                    catch (Exception ex)
+                    {
+                        _telemetryClient.TrackException(ex);
+                        throw;
+                    }
+                    _telemetryClient.StopOperation(operation);
+                    _telemetryClient.Flush();
+                }
+            }
+            catch (LoaderJobException ex)
+            {
+                var body = $"{ex}";
+                Email.SendMail(body, appSettings.EmailSubject, appSettings.EmailFrom, appSettings.EmailTo, appSettings.EmailSmptServer);
+                var job = GetJob(_jobRepository);
+                AddLog(_logCommand, body, ex, job);
+            }
+            catch (Exception ex)
+            {
+                var body = $"{ex}";
+                Email.SendMail(body, appSettings.EmailSubject, appSettings.EmailFrom, appSettings.EmailTo, appSettings.EmailSmptServer);
+
+                var job = GetJob(_jobRepository);
+                if (job == null)
+                {
+                    job = new Job()
+                    {
+                        StatusId = JobStatus.PREVIEW_JOB_FAILED,
+                        StartDate = DateTime.Now,
+                    };
+                    _jobRepository.InsertJob(job);
+                }
+                else
+                {
+                    _jobService?.CompleteJob(job, false);
+                }
+
+                AddLog(_logCommand, body, ex, job);
+            }
+        }
+
+        private static void AddLog(ILogCommand _logCommand, string msg, Exception ex, Job job)
+        {
+            var jobId = job?.Id ?? -1;
+            var log = MyLogger.GetDefaultLog(Guid.Empty, msg, job.Id, LogLevel.Fatal);
+            log.JobID = jobId;
+            log.Exception = ex.ToString();
+            _logCommand.Execute(log);
+        }
+
+        private static Job GetJob(IJobRepository jobRepository)
+        {
+            var job = jobRepository.GetJobListWithStatus()
+                                    .OrderByDescending(j => j.Id)
+                                    .FirstOrDefault();
+            return job;
+        }
+    }
+}
+
